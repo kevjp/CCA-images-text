004084fbcfc5197d3c21f3473580c268549ffd72
kevjp <kevin@ryancodingdesign.com>
2019-09-20 11:33:31 +0100

    Ammended file paths to work on AWS with attached volume

--- load_word2vec.py
+++ load_word2vec.py
@@ -3,7 +3,7 @@ from gensim.models import word2vec
 import logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-sentences = word2vec.Text8Corpus('text8')
+sentences = word2vec.Text8Corpus('/newvolume/text8')
 model = word2vec.Word2Vec(sentences, size=200)
-model.save('text8.model')
-model.wv.save_word2vec_format('text.model.bin', binary=True)
+model.save('/newvolume/text8.model')
+model.wv.save_word2vec_format('/newvolume/text.model.bin', binary=True)
