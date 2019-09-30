b041b05a347992c0742868efdecf7e8d6a196d40
kevjp <kevin@ryancodingdesign.com>
2019-09-30 17:02:59 +0100

    add capacity to observe supersets and subset of groups on scatter diagram

--- main/annotation_scatter.py
+++ main/annotation_scatter.py
@@ -3,21 +3,37 @@ from collections import OrderedDict
 # Generate function which obtains labels for each image based on presence of specific tag returns a list of with a single tag for each image
 ann_dict  = {'kitchen': ['kitchen', 'messy'], 'bathroom': ['bathroom', 'messy'], 'bedroom': ['bedroom', 'messy']}
 def annotate_scatter(top5_array, ann_list=None, ann_dict=None):
+    # variables relating to subset
     ann_out = []
     index_pos = []
     index_count = 0
     add_ann = None
     ann_out_dict = OrderedDict()
+    # variables relating to superset key
+    ann_key = []
+    index_pos_key = []
+    index_count_key = 0
+    add_ann_key = None
+    ann_out_key_dict = OrderedDict()
+
     # Annotate scatter with a list of tags
     if ann_dict is not None:
         for img in top5_array:
             score_vec = 0
+            score_key = 0
             ind_ex = [0]
+            ind_ex_key = [0]
             add_ann = None
             for a in ann_dict:
                 if(set(ann_dict[a]).issubset(set(img[1]))):
                     add_ann = ann_dict[a]
                     ind_ex = [img[1].index(tag) for tag in ann_dict[a]]
+                elif a in set(img[1]):
+                    # generate points for superset relating to key
+                    add_ann_key = a
+                    ind_ex_key = [img[1].index(a)]
+                else:
+                    continue
             if sum(ind_ex) > score_vec:
                 score_vec = sum(ind_ex)
                 ann_out_dict[img[0]] = add_ann
@@ -25,8 +41,17 @@ def annotate_scatter(top5_array, ann_list=None, ann_dict=None):
                 index_count += 1
             else:
                 index_count += 1
+            if ind_ex_key > score_key:
+                score_key = ind_ex_key
+                ann_out_key_dict[img[0]] = add_ann_key
+                index_pos_key.append(index_count_key)
+                index_count_key += 1
+            else:
+                index_count_key += 1
+
         ann_out = [" ".join(ann_out_dict[elem]) for elem in ann_out_dict]
-        return ann_out, index_pos
+        ann_key = [elem for elem in ann_out_key_dict]
+        return ann_out, index_pos, ann_key, index_pos_key
 
     # Annotate scatter with a single tags
     if ann_list is not None:
